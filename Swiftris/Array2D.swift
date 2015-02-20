3ce89b4c1ae3b82cff0c032f000e1ad97e1940ad
Andres C <acubillos@rentpath.com>
2015-02-20 11:37:22 -0500

    swiftris

---
+++ Swiftris/Array2D.swift
@@ -0,0 +1,34 @@
+//
+//  Array2D.swift
+//  Swiftris
+//
+//  Created by acubillos on 2/20/15.
+//  Copyright (c) 2015 rentpath. All rights reserved.
+//
+
+import Foundation
+
+// #1
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    // #2
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        // #3
+        array = Array<T?>(count:rows * columns, repeatedValue: nil)
+    }
+    
+    // #4
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
